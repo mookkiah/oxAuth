6e6fe5d220397006b2f29b362e9a67f09ef28b7c
mookkiah <mahendran.mookkiah@cevalogistics.com>
2021-07-16 06:37:15 -0400

    using JDK native client - using default proxy settings

--- Server/integrations/pingid/src/main/java/org/gluu/oxauth/ping/Utils.java
+++ Server/integrations/pingid/src/main/java/org/gluu/oxauth/ping/Utils.java
@@ -1,29 +1,24 @@
 package org.gluu.oxauth.ping;
 
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import javax.crypto.Mac;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
-import javax.ws.rs.client.ClientRequestContext;
-import javax.ws.rs.client.ClientRequestFilter;
-import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Response;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.HttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.gluu.oxauth.model.crypto.signature.SignatureAlgorithm;
 import org.gluu.oxauth.model.util.Base64Util;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
-import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
-import org.jboss.resteasy.client.jaxrs.engines.ApacheHttpClient4Engine;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.crypto.Mac;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 public class Utils {
 
@@ -32,33 +27,10 @@ public class Utils {
     public static final SignatureAlgorithm HS256_ALG = SignatureAlgorithm.HS256;
     public static final String HMAC_SHA256_ALG_NAME = "HmacSHA256";
 
-    public static ResteasyClient rsClient;
+    private static HttpClient httpClient;
 
     static {
-        PoolingHttpClientConnectionManager manager = new PoolingHttpClientConnectionManager();
-        manager.setMaxTotal(200);
-        manager.setDefaultMaxPerRoute(20);
-
-        RequestConfig config = RequestConfig.custom().setConnectTimeout(10 * 1000).build();
-        HttpClient httpClient = HttpClientBuilder.create().setDefaultRequestConfig(config)
-                .setConnectionManager(manager).build();
-        ApacheHttpClient4Engine engine = new ApacheHttpClient4Engine(httpClient);
-        String proxyHost = System.getProperty("https.proxyHost");
-        String proxyPort = System.getProperty("https.proxyPort");
-        logger.info("proxy {}:{}", proxyHost, proxyPort);
-        if (StringUtils.isNotEmpty(proxyHost) && StringUtils.isNoneEmpty(proxyPort)) {
-            //TODO proxy authentication?
-            logger.info("Using proxy {}:{}", proxyHost, proxyPort);
-            rsClient =
-                    new ResteasyClientBuilder().defaultProxy(proxyHost,
-                            Integer.valueOf(proxyPort)).httpEngine(engine).build();
-            logger.info("Rest Client Properties map: {}",
-                    rsClient.getConfiguration().getProperties());
-        } else {
-            rsClient =
-                    new ResteasyClientBuilder().httpEngine(engine).build();
-        }
-
+        httpClient = HttpClient.newHttpClient();
     }
 
     public static String generateHS256Signature(String input, byte secret[])
@@ -77,28 +49,31 @@ public class Utils {
 
         int status;
         String data;
-        try {
-            ResteasyWebTarget target = rsClient.target(endpoint);
-            logger.info("Sending payload of {} bytes to {}", payload.getBytes().length, endpoint);
+        var request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(payload))
+                .uri(URI.create(endpoint))
+                .build();
+        try{
+            logger.info("HttpClient sending payload of {} bytes to {}", payload.getBytes().length,
+                    endpoint);
             logger.info("{}", payload);
-
-            Response response = target.request().post(Entity.json(payload));
-            status = response.getStatus();
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            data = response.body();
+            status = response.statusCode();
             logger.info("Response code was {}", status);
-            response.bufferEntity();
-            data = response.readEntity(String.class);
-
+            logger.info("Response body:\n{}", data);
             if (status == 200) {
-                logger.info("Response body:\n{}", data);
                 return data;
             }
             logger.error("Response body:\n{}", data);
             throw new HttpException(status, "Unsuccessful response obtained");
-        }catch (Throwable t) { // Ugly - Required because somewhere exception
-            // (ClientTimeoutException) suppressed.
-            t.printStackTrace();
-            throw t;
+        }catch (InterruptedException | IOException e){
+            e.printStackTrace(); // Because somewhere the exception thrown here eaten.
+            throw new HttpException("Unsuccessful response obtained", e);
         }
+
+
+
 
     }
 
